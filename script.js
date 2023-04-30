22a95bc8f331c40aa801c45eb44970f46f0927b1
Flavio Nasuti <flavio.nasuti@gmail.com>
2023-04-30 10:29:44 +0200

    FPS for debug on mobile

--- script.js
+++ script.js
@@ -395,8 +395,10 @@ function gameLoop(currentFrameTimestamp)
 {   
     secondsPassed = (currentFrameTimestamp - previousFrameTimestamp) / 1000
     previousFrameTimestamp = currentFrameTimestamp
-    // For debug
-    //console.log(Math.round(secondsPassed * 3600) + " FPS")
+    // For debug on desktop
+    // console.log(Math.round(secondsPassed * 3600) + " FPS")
+    // For debug on mobile
+    // document.getElementById("fps").innerHTML = Math.round(secondsPassed * 3600) + " FPS"
 
     generatePoison()
     generateFood()
